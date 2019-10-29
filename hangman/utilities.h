603a750d94bb1cd2a724d4a8c8d4f5b410216d4b
Jessica Li <jessicaa.li1212@gmail.com>
2019-10-29 13:49:41 -0400

    Add project files.

---
+++ hangman/utilities.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+void printMessage(string, bool, bool);
+
+string loadRandomWord(string);
+
