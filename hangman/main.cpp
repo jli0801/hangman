48c1044db05e2b9dbb44c29b9e3a8a0effbcea09
Jessica Li <jessicaa.li1212@gmail.com>
2019-11-04 18:42:39 -0500

    changed the function so it can handle all cases, worked on main func, edited the cout

--- hangman/main.cpp
+++ hangman/main.cpp
@@ -9,28 +9,35 @@
 using namespace std;
 
 //variables 
+//GUESSES ARE ALL INPUTS 
 char guesses[26]; //can handle up to 26 guesses
-//guesses are the right letters
+int guessPointer;
+//guesses are the right letters & wrong 
 char misses[6]; //so it can handle up to 6 misses
 //misses are the wrong letters 
-int tries = 6;
+int tries;
 string wordChosen;
-word usersWord = word(wordChosen);
+word usersWord;
 bool won; 
 bool start;
 
 //features
 void start_menu();
-void game_start();
+void updateBoard(); //changing game_start to func as a update board 
+
 bool check_guesses(string input);
 bool check_word(string input);
 
+
+
 int main()
 {
+	//initialize variables 
 	won = false; 
 	start = true;
-	while (!won)
-	{
+	tries = 0; //max is 6 
+	guessPointer = 0; //max is 26
+	
 		if (start)
 		{
 			start_menu();
@@ -41,17 +48,38 @@ int main()
 		{
 			//start the game 
 			wordChosen = loadRandomWord("wordset.txt");
-			game_start();
-			cout << "Guess a letter" << endl;
+			usersWord = word(wordChosen);
+			updateBoard();
+			while (!won)
+			{
+				cout << "Guess a letter/word." << endl;
+				string inputUser;
+				cin >> inputUser; //could be a char or word
+				if (check_guesses(inputUser) && check_word(inputUser))
+				{
+					//they found a letter or word from the chosen word
+					updateBoard();
+				}
+				else
+				{
+					//increment and store the guesses/misses
+					guesses[guessPointer] = inputUser.at(1);
+					guessPointer++;
+					misses[tries] = inputUser.at(1); //CHANGE THIS LATER SO WE CAN TAKE IN MULTIPLE LETTERED WORDS
+					tries++;
+					updateBoard(); //update board 
 
+				}
+			}
+			//when it gets out of the loop you need to prompt the user if they want to start again + tell them if they lost or won
 		}
-	}
+	
 	
 }
 
 bool check_guesses(string input)
 {
-	for (int i = 0; i < 32; i++) {
+	for (int i = 0; i < sizeof(guesses); i++) {
 		//guesses are chars not strings so don't use .compare
 		if (input.at(0) == guesses[i]) {
 			cout << "Already guessed this attempt, please enter another guess";
@@ -79,10 +107,10 @@ void start_menu() {
 	printMessage("Hangman Game", true, false);
 	printMessage("ESE224 Fall 2019", true, false);
 	printMessage("Group Members:", true, false);
-	printMessage("Student A", false, false);
-	printMessage("Student B", false, false);
-	printMessage("Student C", false, false);
-	printMessage("Student D", false, true);
+	printMessage("Jessica Li", false, false);
+	printMessage("Dorothy Lee", false, false);
+	printMessage("Lily Li", false, false);
+	printMessage("Darren Li", false, true);
 	printMessage("Select Difficulty:", false, false);
 	printMessage("1. Easy", false, false);
 	printMessage(" 2. Medium", false, false);
@@ -112,22 +140,61 @@ void start_menu() {
 
 }
 
-void game_start()
+void updateBoard() //changed the function so that it can handle updates 
 {
 	string wordOutput = usersWord.populateWord(usersWord.getWord());
 	printMessage("Hangman", true, false);
 	printMessage("|", true, false);
-	printMessage("", false, false);
-	printMessage("", false, false);
-	printMessage("", false, false);
+	if (guesses[0] != NULL && tries == 1)
+	{
+		printMessage("o", false, false); //head
+		printMessage("", false, false);
+		printMessage("", false, false);
+	}
+	else if (guesses[1] != NULL && tries == 2)
+	{
+		printMessage("o", false, false);
+		printMessage("|", false, false); //body
+		printMessage("", false, false);
+	}
+	else if (guesses[2] != NULL && tries == 3)
+	{
+		printMessage("o", false, false);
+		printMessage("|'\'", false, false); //left arm IDK IF THE SYNTAX RIGHT
+		printMessage("", false, false);
+	}
+	else if (guesses[3] != NULL && tries == 4)
+	{
+		printMessage("o", false, false);
+		printMessage("/|'\'", false, false); //BODY LEFT AND RIGHT ARM
+		printMessage("", false, false);
+	}
+	else if (guesses[4] != NULL && tries == 5)
+	{
+		printMessage("o", false, false);
+		printMessage("/|'\'", false, false); 
+		printMessage("'\'", false, false);	//left foot
+	}
+	else if(guesses[5] != NULL && tries == 6)
+	{
+		printMessage("o", false, false);
+		printMessage("/|'\'", false, false);
+		printMessage("/ '\'", false, false);	//WHOLE BODY
+	}
+	else
+	{
+		printMessage("", false, false); //keep it blank
+		printMessage("", false, false);
+		printMessage("", false, false);
+	}
 	printMessage(wordOutput, true, true );
 	cout << "Guesses: ";
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i < sizeof(guesses);i++)
 	{
 		if (guesses[i] != NULL)
 		{
-			cout << guesses[i];
-			if (i < 99)
+			cout << guesses[i] << " "; //edited so it doesn't mesh together
+			if (i < (sizeof(guesses) -1))
 			{
 				if (guesses[i + 1] == NULL)
 				{
@@ -141,12 +208,12 @@ void game_start()
 	}
 
 	cout << "Misses: ";
-	for (int j = 0; j < 100; j++)
+	for (int j = 0; j < sizeof(misses); j++)
 	{
 		if (misses[j] != NULL)
 		{
-			cout << misses[j];
-			if (j < 99)
+			cout << misses[j] << " ";
+			if (j < (sizeof(misses)-1))
 			{
 				if (misses[j+1] == NULL)
 				{
